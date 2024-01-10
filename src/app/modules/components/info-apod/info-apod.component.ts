0947fde4a5df1c9b0db3bc1cdfde3fa76c170421
esther-romero <202005755@est.umss.edu>
2024-01-10 14:15:39 -0400

    📌 [ADD] skeleton in info-apod

--- src/app/modules/components/info-apod/info-apod.component.ts
+++ src/app/modules/components/info-apod/info-apod.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 import { NasaService } from '../../../core/services/nasa.service';
 
 @Component({
   selector: 'app-info-apod',
   standalone: true,
-  imports: [],
+  imports: [NgxSkeletonLoaderModule],
   templateUrl: './info-apod.component.html',
 })
 export class InfoApodComponent {
   @Input() date!: string;
-  apodData: any;
+  public apodData: any;
+  public apodSkeleton = [1, 2, 3, 4, 5, 6];
+  public isLoading: boolean = true;
 
   constructor(private nasaService: NasaService, private router: Router) {}
 
@@ -20,20 +23,25 @@ export class InfoApodComponent {
   }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.loadData();
+  }
+
+  private loadData() {
+    this.isLoading = true;
     if (!this.date) {
       this.nasaService.getAPOD().subscribe((data) => {
         this.apodData = data;
+        this.isLoading = false;
       });
     } else {
       this.nasaService.getAPODByDate(this.date).subscribe((data) => {
         this.apodData = data;
+        this.isLoading = false;
       });
     }
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.nasaService.getAPODByDate(this.date).subscribe((data) => {
-      this.apodData = data;
-    });
   }
 }
